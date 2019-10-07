a600801b68cf3efa7d339aec3a18276d95724b58
mode12345678 <54635738+mode12345678@users.noreply.github.com>
2019-10-07 18:05:37 +0300

    My services JS #7

--- js/actions.js
+++ js/actions.js
@@ -1,1 +1,2 @@
 renderAchievements( achievements );
+renderServices ( services );
--- js/data.js
+++ js/data.js
@@ -4,3 +4,36 @@ const achievements = [
     ['box', 245, 'Total projects'],
     ['box', 245, 'Total projects']
 ];
+
+const services = [
+    {
+        icon: 'tags', 
+        title: 'Branding', 
+        text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry.', 
+    },
+    {
+        icon: 'laptop', 
+        title: 'WEB DESIGN', 
+        text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry.', 
+    },
+    {
+        icon: 'shopping-basket', 
+        title: 'MARKETING', 
+        text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry.', 
+    },
+    {
+        icon: 'code', 
+        title: 'CLEAN CODE', 
+        text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry.', 
+    },
+    {
+        icon: 'tablet-alt', 
+        title: 'FRIEndLY ALL SCREEN', 
+        text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry.' 
+    },
+    {
+        icon: 'compass', 
+        title: 'UNLIMITED SUPPORT', 
+        text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry.' 
+    }
+];
