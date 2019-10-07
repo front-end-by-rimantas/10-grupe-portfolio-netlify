6a9baeeb10dc4aade23b51306000dbd83bbedc3a
mode12345678 <54635738+mode12345678@users.noreply.github.com>
2019-10-07 20:18:32 +0300

    My blog JS #5

--- js/functions.js
+++ js/functions.js
@@ -24,3 +24,33 @@ let SERVICES = ` <div class="title-white">
             ${HTML}`;  
     return document.getElementById('services').innerHTML = SERVICES;
 }
+
+function renderBlog( data ) {
+    let HTML = '';
+
+for ( let i=0; i<3; i++ ) {
+    HTML += `<div class="item-blog">
+    <div class="img-absolute">
+        <a href="#">${data[i].name}</a>
+    </div>
+    <div class="img-blog">
+        <img src="${data[i].image}" alt="picture 1">
+    </div>
+    <div class="content-blog">
+        <a href="#">
+        <h4>${data[i].title}</h4>
+        </a>
+        <p>${data[i].text}</p>
+        <div class="info-client">
+        <img src="${data[i].icon}" alt="face 1">
+        <p>By: Jack Daniel | 25 june 2017</p>
+        </div>
+    </div>
+</div>`;
+}
+let BLOG = ` <div class="title">
+                <h3><span>latest</span> blog</h3>
+            </div>
+            ${HTML}`;  
+    return document.getElementById('blog').innerHTML = BLOG;
+}
